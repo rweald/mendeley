60a3c4b31b32252f61030550e61d5a3a0beef9a1
Ryan Weald <ryan@weald.com>
2011-04-01 09:23:38 -0700

    remove some commented code to make the class easier to read

--- lib/mendeley/public_api.rb
+++ lib/mendeley/public_api.rb
@@ -25,22 +25,6 @@ module Mendeley
       def self.base_url
         @base_url
       end
-
-      #def request(sub_url, params)
-        #request = build_request_url(sub_url ,params)
-        #JSON.parse(RestClient.get(request))
-      #end
-
-      #def build_request_url(sub_url, params)
-        #request_url = File.join(self.class.base_url, sub_url)
-        #param_string = "?consumer_key=#{Mendeley.consumer_key}"
-        #params.each do |key,value|
-          #param_string << "&#{key}=#{value}"
-        #end
-        #request_url.concat(param_string)
-      #end
-      #private :build_request_url
-
     end
   end
 end
