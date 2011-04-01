709687fc3a151b931b1ade1fe5fae09f1d9cc0d3
Ryan Weald <ryan@weald.com>
2011-03-31 23:14:09 -0700

    add consumer_key as a Module instance variable and accompanying test

--- lib/mendeley.rb
+++ lib/mendeley.rb
@@ -1,4 +1,7 @@
 require 'mendeley/public_api'
 module Mendeley
-
+  #Setup the consumer_key that will be used for all public api requests
+  class << self
+    attr_accessor :consumer_key
+  end
 end
--- spec/mendeley_spec.rb
+++ spec/mendeley_spec.rb
@@ -8,4 +8,12 @@ describe "Mendeley" do
   it "should have a API submodule" do
     Mendeley.constants.include?("API")
   end
+
+  it "should have a consumer key" do
+    Mendeley.singleton_methods.include?("consumer_key")
+  end
+
+  it "should have nil as the initial consumer key value" do
+    Mendeley.consumer_key.should be_nil
+  end
 end
