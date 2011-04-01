3be7cac9f4082437bc6671a55bd0e92f5e666814
Ryan Weald <ryan@weald.com>
2011-03-31 23:42:22 -0700

    clean out some unnecessary module reference nesting in the test suite

--- spec/mendeley/public_api_spec.rb
+++ spec/mendeley/public_api_spec.rb
@@ -1,12 +1,10 @@
+require "spec_helper"
+include Mendeley::API
 describe "Mendeley" do
-  before(:each) do
-    include Mendeley
-    #include API
-  end
   describe "API" do
     describe "Documents" do
       it "should have a base_url" do
-        Mendeley::API::Documents.base_url.should == "http://api.mendeley.com/oapi/documents/"
+        Documents.base_url.should == "http://api.mendeley.com/oapi/documents/"
       end
     end
   end
